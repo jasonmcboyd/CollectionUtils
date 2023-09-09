6a6fde54a1e104dfc4b7e132893af8d3fb413958
Jason Boyd <jason.boyd.ce@gmail.com>
2023-09-08 17:31:06 -0700

    Removed reflection from the build process

--- scripts/create-module-manifest.ps1
+++ scripts/create-module-manifest.ps1
@@ -22,32 +22,6 @@ function GetModuleFiles {
   "$ModuleName.psd1"
 }
 
-function LoadAssemblies {
-  param (
-    [string]
-    $ModuleFolder
-  )
-
-  Get-ChildItem -Path $ModuleFolder *.dll `
-  | ForEach-Object { [System.Reflection.Assembly]::LoadFrom($_.FullnName) }
-}
-
-function GetModuleFunctions {
-  param (
-    [string]
-    $ModuleFolder,
-
-    [string]
-    $ModuleName
-  )
-
-  $assemblyPath = Join-Path $ModuleFolder "$ModuleName.dll"
-  $assembly = [System.Reflection.Assembly]::LoadFrom($assemblyPath)
-  $types = $assembly.GetTypes().Where({ [System.Management.Automation.PsCmdlet].IsAssignableFrom($_) })
-
-  $types.GetCustomAttributes([System.Management.Automation.CmdletAttribute], $false).Foreach({ "$($_.VerbName)-$($_.NounName)" })
-}
-
 function GetVersionNumber {
   param (
     [int]
@@ -70,18 +44,18 @@ $fileList =
     -ModuleFolder $publishVariables.ModuleFolder `
     -ModuleName $publishVariables.ModuleName
 
-# LoadAssemblies
-
-$functionsToExport =
-  GetModuleFunctions `
-    -ModuleFolder $publishVariables.ModuleFolder `
-    -ModuleName $publishVariables.ModuleName
-
 $version =
   GetVersionNumber `
     -MajorVersion $publishVariables.MajorVersion `
     -MinorVersion $publishVariables.MinorVersion `
     -PatchVersion $PatchVersion
+
+$functionsToExport = @(
+  'ConvertFrom-DataTable'
+  'ConvertTo-DataTable'
+  'Join-Object'
+  'Test-Object'
+)
 
 New-ModuleManifest `
   -Path $publishVariables.ManifestPath `
